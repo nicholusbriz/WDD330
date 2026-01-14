086371a4a3bf6d0cf05033a8435d43e26d9ce3e6
stevenmondragon23 <smondragonflores@byupathway.edu>
2026-01-14 10:17:42 -0500

    Fix cart item quantity handling and rendering

--- src/js/product.js
+++ src/js/product.js
@@ -1,4 +1,3 @@
-
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
@@ -6,10 +5,18 @@ const dataSource = new ProductData("tents");
 function addProductToCart(product) {
   const cart = JSON.parse(localStorage.getItem("so-cart") || "[]");
 
-  cart.push(product);
+  const itemIndex = cart.findIndex((item) => item.Id === product.Id);
+
+  if (itemIndex >= 0) {
+    cart[itemIndex].qty += 1;
+  } else {
+    product.qty = 1;
+    cart.push(product);
+  }
 
   localStorage.setItem("so-cart", JSON.stringify(cart));
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
@@ -20,4 +27,3 @@ async function addToCartHandler(e) {
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
-
